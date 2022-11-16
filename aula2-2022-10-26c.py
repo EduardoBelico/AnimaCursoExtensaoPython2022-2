b678c428b09044cc3c38434c7b96476000c1a643
Eduardo Bélico <116230274+EduardoBelico@users.noreply.github.com>
2022-11-16 15:05:24 +0000

    Exemplo de while e lista com while

---
+++ aula2-2022-10-26c.py
@@ -0,0 +1,18 @@
+# Exemplo de laço
+# Se eu quiser exibir números de 1 a 10
+print("Exibindo da maneira 'rústica'")
+print("1")
+print("2")
+print("3")
+print("4")
+print("5")
+print("6")
+print("7")
+print("8")
+print("9")
+print("10")
+
+print("Exibindo da maneira certa")
+numero = 1
+print(numero)
+numero = numero + 1
---
+++ aula3-2022-11-09.py
@@ -0,0 +1,28 @@
+# Aprendendo laços!
+
+contador = 1
+
+# Exibir de 1 até 10 repetidamente
+while(contador <= 10):
+  print(contador)
+  contador = contador + 1 # contador += 1
+
+# Laço for (em python for = for each)
+fruta = "morango"
+print(fruta)
+
+# Lista e mostrar a terceira fruta
+frutas = ["morango", "laranja", "limão"]
+print(frutas[1])
+
+# Adicionar itens à lista
+frutas.append("manga")
+
+# Quantas frutas tem na minha lista?
+print(len(frutas)) # length = tamanho
+print(frutas)
+
+i=0
+while(i<len(frutas)):
+  print(frutas[i])
+  i = i + 1
